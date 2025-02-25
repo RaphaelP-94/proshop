066608f66d81bed4d64e26b2b1ab34a58b58395c
Raphael <petsch.ra@gmail.com>
2025-02-25 09:59:59 +0100

    Icon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
-import {APP_NAME, APP_DESCRIPTION} from '@/lib/constants';
-import { ThemeProvider } from "next-themes";
+import { APP_NAME, APP_DESCRIPTION } from '@/lib/constants';
+import { ThemeProvider } from 'next-themes';
 
-const inter = Inter({subsets: ['latin']});
-
-
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: {
@@ -15,7 +12,6 @@ export const metadata: Metadata = {
     default: APP_NAME,
   },
   description: APP_DESCRIPTION,
-  
 };
 export default function RootLayout({
   children,
@@ -24,11 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange enableSystem>
-        {children}
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          disableTransitionOnChange
+          enableSystem
+        >
+          {children}
         </ThemeProvider>
       </body>
     </html>
